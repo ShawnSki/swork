503549c94a5330861021a953aafd0f3e3448dfcd
shawn's macbook <shawnscivally@shawn-ski.local>
2019-05-09 10:56:39 -0600

    moved State to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import ChoresDisplay from './components/ChoresDisplay';
 
 
-function App() {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            chores: [],
+            test: 1
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/api/chores').then((res) => {
+            // console.log(res.data)
+            this.setState({
+                chores: res.data
+            })
+        })
+    }
+
+    render() {
   return (
     <div className="App">
-    <ChoresDisplay />
+    <ChoresDisplay choreList={this.state.chores} />
     </div>
   );
+}
 }
 
 export default App;
--- src/components/ChoresDisplay.js
+++ src/components/ChoresDisplay.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import './ChoresDisplay.css'
 
+
+
 class ChoresDisplay extends Component {
-    constructor() {
-        super()
-        this.state = {
-            chores: [],
-            test: 1
-        }
-    }
-
-    componentDidMount() {
-        axios.get('/api/chores').then((res) => {
-            // console.log(res.data)
-            this.setState({
-                chores: res.data
-            })
-        })
-    }
-
-
 
     render() {
         // This is mapping over the chores list and displaying each item
-        const choresMapped = this.state.chores.map((choreObj) => {
+        const choresMapped = this.props.choreList.map((choreObj) => {
             return <div className='choreItem' key={choreObj.id}>
                 <div className='btnBox'>
                     <button>X</button>
@@ -35,11 +18,8 @@ class ChoresDisplay extends Component {
             </div>
         })
         // console.log(chores[1].id)
-
         return (
-
             <div>{choresMapped}</div>
-
         )
     }
 }

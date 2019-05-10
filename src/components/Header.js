1a48d50f7350eb3b8bead6a29f025e712e8be9d7
shawn's macbook <shawnscivally@shawn-ski.lan>
2019-05-09 21:29:28 -0600

    header styling added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Header.css'
+var imageName = require('../spouseworklogo.png')
 
 
 function Header(props) {
- 
-        return (
-            <div className='headerItem'>
-            <div>logo</div>
-                <div>
-                <form onSubmit={props.handleAddChore}>
-                <input className='inputChore' placeholder='Add a new item...' name='newChoreListing' onChange={props.handleUpdateInput} />
-                <input className='inputPoints' placeholder='Points' name='newChorePoints' onChange={props.handleUpdateInput} />
-                <button>send</button>
-                </form>
-                </div>
+
+    return (
+        <div className='headerItem'>
+            <div>
+                <img src={imageName} />
             </div>
-        );
-    
+            <div className='headerContainer'>
+                <form onSubmit={props.handleAddChore}>
+                    <input className='inputChore' placeholder='Add a new item...' name='newChoreListing' onChange={props.handleUpdateInput} />
+                    <input className='inputPoints' placeholder='Points' name='newChorePoints' onChange={props.handleUpdateInput} />
+                    <button>POST</button>
+                </form>
+            </div>
+        </div>
+    );
+
 }
 
 export default Header;
